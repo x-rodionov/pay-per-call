fffcb41f0df44cb003e386f3c404be0955f0c045
Lev Chelyadinov <leva181777@gmail.com>
2022-07-02 19:17:34 +0300

    Add the logo to the header

--- src/components/header/header.svelte
+++ src/components/header/header.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+  import Branding from './branding.svelte';
   import { Auth } from '../auth';
   import {TeachSelector} from '../teach-selector';
 </script>
 
-<header>
-  <h1>Diamonds</h1>
+<header class="flex justify-between items-center px-12 lg:px-40 py-12">
+  <Branding />
   <TeachSelector />
   <Auth />
 </header>
