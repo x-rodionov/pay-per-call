1788dc50b88259084b4b7e445d7ac39eb4a2aee2
Lev Chelyadinov <leva181777@gmail.com>
2022-07-02 21:18:20 +0300

    Keep just the landing on the landing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-	import { UserList } from '../components/user-list';
 	import { LandingPage } from '$lib/pages/landing';
 </script>
 
-<section>
-	<h2>User list</h2>
-
-	<UserList />
-</section>
 <LandingPage />
