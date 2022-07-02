426b598abd97e880a36ca6be9af5fca4d859dd90
Lev Chelyadinov <leva181777@gmail.com>
2022-07-02 19:35:11 +0300

    Add more stuff to the UI kit

--- src/lib/shared/ui/index.ts
+++ src/lib/shared/ui/index.ts
@@ -1,3 +1,5 @@
 export { default as CTAButton } from './cta-button.svelte';
+export { default as OutlineButton } from './outline-button.svelte';
 export { default as TextField } from './text-field.svelte';
+export { default as H1 } from './h1.svelte';
 export { default as FlatIconButton } from './flat-icon-button.svelte';
