13256b050b284a8b4f957b04727461342b43e01e
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-07-02 23:02:30 +0300

    Fix peer ID getter

--- src/lib/features/auth/api/connect.ts
+++ src/lib/features/auth/api/connect.ts
@@ -3,16 +3,12 @@ import Peer from 'peerjs';
 import { peer as peerStore, user as userStore, wallet as walletStore } from '$lib/entities/user';
 
 import { createUser, findUser } from '../../../../utils/users';
-import { getPeerId } from '../../../../utils/get-peer-id';
+import { getPeerId } from '$lib/entities/user';
 import { getKeyPairAndWallet } from '../../../../utils/ton';
 
 export const connect = async (name: string, mnemonic: string[]) => {
 	const ton = await getKeyPairAndWallet(mnemonic);
-	const peer = new Peer(getPeerId(name), {
-		host: 'ec2-52-59-224-143.eu-central-1.compute.amazonaws.com',
-		port: 9000,
-		path: '/myapp'
-	});
+
 
 	let user = await findUser(name);
 	if (!user) {
@@ -21,6 +17,12 @@ export const connect = async (name: string, mnemonic: string[]) => {
 			throw new Error("Couldn't create a user");
 		}
 	}
+
+	const peer = new Peer(getPeerId(user), {
+		host: 'ec2-52-59-224-143.eu-central-1.compute.amazonaws.com',
+		port: 9000,
+		path: '/myapp'
+	});
 
 	peerStore.set(peer);
 	userStore.set(user);
