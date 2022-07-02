ed40388a2a4e7f0b9a85df67e7cb05f3d4fe512b
Lev Chelyadinov <leva181777@gmail.com>
2022-07-02 19:16:44 +0300

    Implement flat icon buttons

---
+++ src/lib/shared/ui/flat-icon-button.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import clsx from 'clsx';
+
+  let _class = '';
+  export { _class as class };
+
+  export let label: string;
+  export let icon: any;
+</script>
+
+<button class={clsx("text-blue-500", "p-2", "rounded-full", "hover:bg-blue-500/10", _class)} aria-label={label} title={label} on:click>
+  <svelte:component this={icon} width={24} height={24} />
+</button>
--- src/lib/shared/ui/index.ts
+++ src/lib/shared/ui/index.ts
@@ -1,2 +1,3 @@
 export { default as CTAButton } from './cta-button.svelte';
 export { default as TextField } from './text-field.svelte';
+export { default as FlatIconButton } from './flat-icon-button.svelte';
