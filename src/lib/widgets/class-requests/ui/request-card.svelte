debf8652d9090aae9238d060d43f7548415120da
Lev Chelyadinov <leva181777@gmail.com>
2022-07-02 21:18:37 +0300

    Format the request card file

--- src/lib/widgets/class-requests/ui/request-card.svelte
+++ src/lib/widgets/class-requests/ui/request-card.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-import { CTAButton, OutlineButton } from "$lib/shared/ui";
+	import { CTAButton, OutlineButton } from '$lib/shared/ui';
 
-  export let name: string;
+	export let name: string;
 </script>
 
 <article class="flex justify-between items-center py-4 border-b border-gray-400">
-  <h2 class="text-xl">{name}</h2>
-  <div class="flex">
-    <CTAButton class="mr-2">Join</CTAButton>
-    <OutlineButton>Cancel</OutlineButton>
-  </div>
+	<h2 class="text-xl">{name}</h2>
+	<div class="flex">
+		<CTAButton class="mr-2">Join</CTAButton>
+		<OutlineButton>Cancel</OutlineButton>
+	</div>
 </article>
