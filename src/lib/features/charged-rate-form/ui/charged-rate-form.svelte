0a80745a741f9c7f912f3aaa123a9977388a6665
Lev Chelyadinov <leva181777@gmail.com>
2022-07-02 19:42:36 +0300

    Add tutor profile forms

---
+++ src/lib/features/charged-rate-form/ui/charged-rate-form.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { TextField, OutlineButton } from '$lib/shared/ui';
+
+	// Trust me, I know better than TS
+	let rate = 1 as unknown as string;
+</script>
+
+<form class="flex items-end" on:submit|preventDefault={() => console.log(rate)}>
+	<TextField
+		id="rate-field"
+		type="number"
+		label="How much do you charge per minute in nTON?"
+		bind:value={rate}
+	/>
+	<OutlineButton type="submit" class="ml-2" disabled>Save</OutlineButton>
+</form>
---
+++ src/lib/pages/tutor-dashboard/ui/tutor-dashboard-page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { H1 } from '$lib/shared/ui';
+
+  import { ExpertiseForm } from '$lib/features/expertise-form';
+  import { ChargedRateForm } from '$lib/features/charged-rate-form';
+	// import { Header } from '$lib/widgets/header';
+</script>
+
+<!-- <Header /> -->
+<main class="px-12 lg:px-40">
+	<H1>Tutor profile</H1>
+
+	<div class="flex justify-between">
+		<ExpertiseForm />
+		<ChargedRateForm />
+	</div>
+</main>
