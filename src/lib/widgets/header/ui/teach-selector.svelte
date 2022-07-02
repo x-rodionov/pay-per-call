f8784dfb24e543d62138f5bc66a496bd43b07151
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-07-03 00:58:57 +0300

    Undo setting teaching store and use the page store

--- src/lib/widgets/header/ui/teach-selector.svelte
+++ src/lib/widgets/header/ui/teach-selector.svelte
@@ -1,22 +1,18 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
 	import { LinklikeTab } from '$lib/shared/ui';
 
 	import { isTeaching } from '../model/is-teaching';
-
-	onMount(() => {
-		isTeaching.set(window.location.pathname === '/tutor');
-	});
 </script>
 
 <div>
 	<label>
 		<input class="sr-only" type="radio" name="teach-mode" value={true} bind:group={$isTeaching} />
-		<LinklikeTab active={$isTeaching}>Teach</LinklikeTab>
+		<LinklikeTab active={$page.url.pathname === '/tutor'}>Teach</LinklikeTab>
 	</label>
 	<span class="mx-1">/</span>
 	<label>
 		<input class="sr-only" type="radio" name="teach-mode" value={false} bind:group={$isTeaching} />
-		<LinklikeTab active={!$isTeaching}>Learn</LinklikeTab>
+		<LinklikeTab active={$page.url.pathname === '/student'}>Learn</LinklikeTab>
 	</label>
 </div>
