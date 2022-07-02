0ff6c27a20e6bf05188f5bcac1e84d9a1d10ae22
Lev Chelyadinov <leva181777@gmail.com>
2022-07-03 02:54:29 +0300

    Fix the peer ID generation

--- src/lib/entities/user/lib/get-peer-id.ts
+++ src/lib/entities/user/lib/get-peer-id.ts
@@ -2,5 +2,5 @@ import type { User } from "../type";
 import { APP_NAME } from '$lib/shared/lib/env';
 
 export const getPeerId = (user: User) => {
-	return `${APP_NAME}-${user.wallet_id}`;
+	return `${APP_NAME}-${user.wallet_id.replace('-', '-a').replace('_', '_a')}`;
 };
