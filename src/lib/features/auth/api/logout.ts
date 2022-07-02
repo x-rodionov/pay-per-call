87693f59539f73b17cf98aeca3dc6e878633a7d9
Lev Chelyadinov <leva181777@gmail.com>
2022-07-03 02:55:16 +0300

    Properly destroy the Peer object on logout

--- src/lib/features/auth/api/logout.ts
+++ src/lib/features/auth/api/logout.ts
@@ -1,10 +1,13 @@
+import { get } from 'svelte/store';
 import { goto } from '$app/navigation';
+
 import { peer as peerStore, user as userStore, wallet as walletStore } from '$lib/entities/user';
 import { removeKeys } from '$lib/shared/api/ton';
 
 export const logout = () => {
 	removeKeys();
 
+	get(peerStore)?.destroy();
 	peerStore.set(null);
 	userStore.set(null);
 	walletStore.set(null);
