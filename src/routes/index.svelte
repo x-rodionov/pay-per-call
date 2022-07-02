af28b8d574d0fa5fbd324bc2a2544bb2a5eff35d
VanishMax <maxkors2014@gmail.com>
2022-07-02 16:45:21 +0300

    refactor: move Call page to Home

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import {UserList} from '../components/user-list';
+</script>
+
+<section>
+  <h2>User list</h2>
+  
+  <UserList />
+</section>
