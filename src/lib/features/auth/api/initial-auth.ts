faf256175f43502f9516e4510e4d2fbf058bbf65
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-07-03 00:45:27 +0300

    Fetch user when wallet was already persisted

--- src/lib/features/auth/api/initial-auth.ts
+++ src/lib/features/auth/api/initial-auth.ts
@@ -1,5 +1,6 @@
 import { getPersistedWallet } from '$lib/shared/api/ton';
-import { wallet as walletStore } from '$lib/entities/user';
+import { wallet as walletStore, user as userStore } from '$lib/entities/user';
+import { findUser } from '$lib/shared/api/users';
 
 export const initialAuth = () => {
 	const data = getPersistedWallet();
@@ -8,9 +9,12 @@ export const initialAuth = () => {
 	// Populate the wallet address field
 	// Not sure if this will cause race conditions. It might be better to transform into an async function
 	const { wallet } = data;
-	wallet.getAddress().then((address) => {
+	wallet.getAddress().then(async (address) => {
 		wallet.address = address;
+		const addressString = address.toString(true, true, true);
+		const user = await findUser(addressString);
 		walletStore.set(wallet);
+		userStore.set(user);
 	});
 
 	return true;
