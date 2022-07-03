1de09330dbbc32bf386d08bf4c614cc2df995b7f
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-07-03 03:31:09 +0300

    Display video streams in class page

--- src/routes/class.svelte
+++ src/routes/class.svelte
@@ -1,3 +1,37 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import { streams } from '$lib/entities/stream';
+    import { onMount } from 'svelte';
 
-<div />
+    let myVideoEl: HTMLVideoElement;
+    let tutorVideoEl: HTMLVideoElement;
+    onMount(() => {
+        if ($streams == null) {
+            throw new Error('$streams is null');
+        };
+        const [myStream, tutorStream] = $streams;
+        tutorVideoEl.srcObject = tutorStream;
+        tutorVideoEl.play();
+        myVideoEl.srcObject = myStream;
+        myVideoEl.play();
+    })
+</script>
+
+<!-- svelte-ignore a11y-media-has-caption -->
+<video bind:this={tutorVideoEl} class="tutor-video"></video>
+<!-- svelte-ignore a11y-media-has-caption -->
+<video bind:this={myVideoEl} class="my-video" muted></video>
+
+<style>
+    video {
+        position: absolute;
+    }
+    .tutor-video {
+        width: 100%;
+        height: 100%;
+    }
+    .my-video {
+        height: 25%;
+        bottom: 0;
+        right: 10%;
+    }
+</style>
