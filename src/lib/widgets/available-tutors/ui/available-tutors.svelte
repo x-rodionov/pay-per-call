41cd19f83d5434bc1d4585c1a3504f998bed57ee
Lev Chelyadinov <leva181777@gmail.com>
2022-07-03 13:32:23 +0300

    Add empty state for tutors

--- src/lib/widgets/available-tutors/ui/available-tutors.svelte
+++ src/lib/widgets/available-tutors/ui/available-tutors.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
 	import { initiateClass } from '$lib/features/initiate-class';
 	import { availableTutors, TutorMinicard } from '$lib/entities/tutor';
+	import { user } from '$lib/entities/user';
 	import { H1 } from '$lib/shared/ui';
 </script>
 
 <div class="px-12 lg:px-40">
 	<H1>Available tutors</H1>
 	<div class="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
-		{#each $availableTutors as tutor}
+		{#each $availableTutors.filter(tutor => tutor.id !== $user?.id) as tutor}
 			<TutorMinicard {tutor} on:request={() => initiateClass(tutor)} />
+		{:else}
+			<div class="col-span-full">
+				<p class="text-gray-500">No tutors available yet</p>
+			</div>
 		{/each}
 	</div>
 </div>
