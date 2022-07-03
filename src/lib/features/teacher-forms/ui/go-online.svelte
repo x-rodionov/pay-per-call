43965e23f6c7de676de3f73d199acb5e9e4e2d9d
Lev Chelyadinov <leva181777@gmail.com>
2022-07-03 03:21:07 +0300

    Add null safety for the online widget

--- src/lib/features/teacher-forms/ui/go-online.svelte
+++ src/lib/features/teacher-forms/ui/go-online.svelte
@@ -2,14 +2,16 @@
   import { CTAButton } from '$lib/shared/ui';
   import { user } from '$lib/entities/user';
   import { changeUserField } from '../../../shared/api/users';
-  
+
   const go = async (online: boolean) => {
-    await changeUserField($user!.wallet_id, { field: 'online', value: online });
-    $user.online = online;
+    if ($user !== null) {
+      await changeUserField($user!.wallet_id, { field: 'online', value: online });
+      $user.online = online;
+    }
   };
 </script>
 
-{#if $user.online}
+{#if $user?.online}
   <p>You are online. Go offline to stop receiving requests for classes</p>
   <CTAButton class="mt-2" on:click={() => go(false)}>Go offline</CTAButton>
 {:else}
