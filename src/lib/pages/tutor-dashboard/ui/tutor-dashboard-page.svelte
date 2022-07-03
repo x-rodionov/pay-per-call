77f3af521b8f88995d33546bb9f85937ca107017
Lev Chelyadinov <leva181777@gmail.com>
2022-07-03 06:36:27 +0300

    Move the content of the tutor dashboard to `pages/`

--- src/lib/pages/tutor-dashboard/ui/tutor-dashboard-page.svelte
+++ src/lib/pages/tutor-dashboard/ui/tutor-dashboard-page.svelte
@@ -1,18 +1,31 @@
 <script lang="ts">
-	import { H1 } from '$lib/shared/ui';
-
+	import { Header } from '$lib/widgets/header';
 	import { ClassRequests } from '$lib/widgets/class-requests';
-  import { ExpertiseForm } from '$lib/features/expertise-form';
-  import { ChargedRateForm } from '../../../features/teacher-forms';
+	import { ChargedRateForm, ExpertiseForm, GoOnline } from '$lib/features/teacher-forms';
+  import { user } from '$lib/entities/user';
+  import { H1 } from '$lib/shared/ui';
 </script>
 
 <main class="px-12 lg:px-40 pb-6">
-	<H1>Tutor profile</H1>
-	<div class="flex justify-between">
-		<ExpertiseForm />
-		<ChargedRateForm />
-	</div>
+	<Header />
 
-	<H1>Requests for classes</H1>
-	<ClassRequests />
+	<section class="pt-12 px-12 lg:px-40">
+		<H1>Tutor profile</H1>
+
+		<div class="flex justify-between">
+			<ExpertiseForm />
+			<ChargedRateForm />
+		</div>
+
+
+		<div class="mt-8">
+			<GoOnline />
+		</div>
+
+		{#if $user?.online}
+			<div class="mt-16">
+				<ClassRequests />
+			</div>
+		{/if}
+	</section>
 </main>
--- src/routes/tutor.svelte
+++ src/routes/tutor.svelte
@@ -1,29 +1,5 @@
 <script lang="ts">
-  import { H1 } from '$lib/shared/ui';
-	import { Header } from '$lib/widgets/header';
-	import { ClassRequests } from '$lib/widgets/class-requests';
-	import { ChargedRateForm, ExpertiseForm, GoOnline } from '$lib/features/teacher-forms';
-  import { user } from '$lib/entities/user';
+  import { TutorDashboardPage } from '$lib/pages/tutor-dashboard';
 </script>
 
-<Header />
-
-<section class="pt-12 px-12 lg:px-40">
-  <H1>Teacher profile</H1>
-  
-  <div class="flex justify-between">
-    <ExpertiseForm />
-    <ChargedRateForm />
-  </div>
-  
-  
-  <div class="mt-8">
-    <GoOnline />
-  </div>
-  
-  {#if $user?.online}
-    <div class="mt-16">
-      <ClassRequests />
-    </div>
-  {/if}
-</section>
+<TutorDashboardPage />
