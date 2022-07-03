200d5fe6b631c6caa073520c8be6a625c206cf92
Lev Chelyadinov <leva181777@gmail.com>
2022-07-03 12:22:08 +0300

    Make proper hangups

--- src/lib/pages/classroom/ui/classroom-page.svelte
+++ src/lib/pages/classroom/ui/classroom-page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import IconCallEnd from '~icons/material-symbols/call-end-outline';
-  import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import IconCallEnd from '~icons/material-symbols/call-end-outline';
+	import { goto } from '$app/navigation';
 
-  import { activeCall } from '$lib/entities/peer';
+	import { activeCall, activeConnection } from '$lib/entities/peer';
 	import { streams } from '$lib/entities/stream';
-  import { user } from '$lib/entities/user';
-  import { CTAButton } from '$lib/shared/ui';
+	import { user } from '$lib/entities/user';
+	import { CTAButton } from '$lib/shared/ui';
+	import { classStateTutor, CSTR } from '$lib/entities/class';
 
 	let myVideoEl: HTMLVideoElement;
 	let tutorVideoEl: HTMLVideoElement;
@@ -22,40 +23,56 @@
 		myVideoEl.play();
 	});
 
-  function hangUp() {
-    if ($activeCall !== null) {
-      $activeCall.close();
-      if ($streams !== null) {
-        const [myStream, tutorStream] = $streams;
-        myStream.getTracks().forEach(track => track.stop());
-        tutorStream.getTracks().forEach(track => track.stop());
-      }
-      if ($user !== null) {
-        goto($user.id === $activeCall.metadata.tutor.id ? '/tutor' : '/student');
-      }
-    }
-  }
+	$: isTutor = $activeCall && $user && $user?.id === $activeCall?.metadata.tutor.id;
+
+	function hangUp() {
+		if ($activeCall !== null && $activeConnection !== null) {
+			$activeCall.close();
+			$activeConnection.close();
+			if (isTutor) {
+				classStateTutor.set(CSTR.CLOSING);
+			}
+			if ($streams !== null) {
+				const [myStream, tutorStream] = $streams;
+				myStream.getTracks().forEach((track) => track.stop());
+				tutorStream.getTracks().forEach((track) => track.stop());
+			}
+			if (isTutor !== null) {
+				goto(isTutor ? '/tutor' : '/student');
+			}
+		}
+	}
 </script>
 
 <div class="flex-1 flex flex-col justify-between before:w-0">
-  <main class="relative mx-12 lg:mx-40 my-10">
-    <!-- svelte-ignore a11y-media-has-caption -->
-    <video bind:this={tutorVideoEl} class="w-full h-full bg-gray-600 rounded-lg" />
-    <!-- svelte-ignore a11y-media-has-caption -->
-    <video bind:this={myVideoEl} class="absolute h-1/4 bottom-4 right-4 bg-green-600 rounded-md" muted />
-  </main>
-  {#if $activeCall !== null}
-    <div class="py-6 flex justify-center items-center space-x-10">
-      <span class="font-bold">{$activeCall.metadata.tutor.course}</span>
-      <span>13:37 / 60:00</span>
-      <CTAButton on:click={hangUp} class="px-12 py-4 bg-orange-500 hover:bg-orange-600 focus:ring-orange-200 dark:bg-orange-300 dark:hover:bg-orange-400 dark:focus:ring-orange-800">
-        <IconCallEnd class="w-8 h-8" />
-      </CTAButton>
-    </div>
-  {:else}
-    <div class="py-6 flex justify-center items-center">
-      <p>No active call. <a href="/student" class="underline text-blue-500 dark:text-blue-300">Go back</a></p>
-    </div>
-  {/if}
+	<main class="relative mx-12 lg:mx-40 my-10">
+		<!-- svelte-ignore a11y-media-has-caption -->
+		<video bind:this={tutorVideoEl} class="w-full h-full bg-gray-600 rounded-lg" />
+		<!-- svelte-ignore a11y-media-has-caption -->
+		<video
+			bind:this={myVideoEl}
+			class="absolute h-1/4 bottom-4 right-4 bg-green-600 rounded-md"
+			muted
+		/>
+	</main>
+	{#if $activeCall !== null}
+		<div class="py-6 flex justify-center items-center space-x-10">
+			<span class="font-bold">{$activeCall.metadata.tutor.course}</span>
+			<span>13:37 / 60:00</span>
+			<CTAButton
+				on:click={hangUp}
+				class="px-12 py-4 bg-orange-500 hover:bg-orange-600 focus:ring-orange-200 dark:bg-orange-300 dark:hover:bg-orange-400 dark:focus:ring-orange-800"
+			>
+				<IconCallEnd class="w-8 h-8" />
+			</CTAButton>
+		</div>
+	{:else}
+		<div class="py-6 flex justify-center items-center">
+			<p>
+				No active call. <a href="/student" class="underline text-blue-500 dark:text-blue-300"
+					>Go back</a
+				>
+			</p>
+		</div>
+	{/if}
 </div>
-
