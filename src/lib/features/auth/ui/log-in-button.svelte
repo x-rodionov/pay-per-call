b321104de8811cc9e11e3f9205b110f4fca41359
Lev Chelyadinov <leva181777@gmail.com>
2022-07-03 03:18:30 +0300

    Reconcile the styles of the CTA and the login buttons

--- src/lib/features/auth/ui/log-in-button.svelte
+++ src/lib/features/auth/ui/log-in-button.svelte
@@ -1,18 +1,5 @@
-<button
-	type="button"
-	class="
-    text-white
-    bg-blue-500 hover:bg-blue-600
-    shadow
-    focus:ring-4 focus:ring-blue-200 focus:outline-none
-    font-semibold text-lg font-roboto-slab
-    px-12 py-4
-    mr-2 mb-2
-    dark:bg-blue-300 dark:hover:bg-blue-400
-    dark:focus:ring-blue-800
-    rounded
-  "
-  on:click
->
-  Log in with a TON wallet
-</button>
+<script lang="ts">
+	import { CTAButton } from '$lib/shared/ui';
+</script>
+
+<CTAButton class="px-12 py-4" on:click>Log in with a TON wallet</CTAButton>
